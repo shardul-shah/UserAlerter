fe7a3943200b7a3dd99cd0b04effb1ec10112e37
Shardul Shah <shardul@ualberta.ca>
2021-08-30 21:12:51 -0600

    Some progress, setting up env usage for credentials, updating requirements.txt and email worker

--- emailWorker.py
+++ emailWorker.py
@@ -4,23 +4,44 @@ import smtplib
 import ssl
 
 # Here are the email package modules we"ll need
-from email.mime.image import MIMEImage
+
+# from email.mime.image import MIMEImage # not needed for now
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import sys
+
+import os
+
+from dotenv import load_dotenv
 
 def createEmail():
 	# Create the container (outer) email message.
-	message = MIMEMultipart("hello\nthis is a test email\n")
+
+	# By default the type is set to mixed
+	message = MIMEMultipart()
 	print("\n\n", message)
-	message["Subject"] = "Hi Urmi"
+	message["Subject"] = "Hi"
 	# me == the sender"s email address
 	# family = the list of all recipients" email addresses
-	message["From"] = "testinguseralerterapp@gmail.com" # message["From"] contains the sender's email address
-	recipient_list = ["intentionallycommittingfakeemails@gmail.com", "tohidetestingemails@gmail.com"]
+	message["From"] = "User Alerter <testinguseralerterapp@gmail.com>" # message["From"] contains the sender's email address
+	recipient_list = ["the.shardul.shah@gmail.com"]
 	message["To"] = ", ".join(recipient_list) # message["To"] contains the recipient (one or many)'s email addresses, in a comma separated list
-	message.preamble = "Test 2"
+	# If a Reply-To is explicitly provided then the email client will
+	# use this address when the user hit the reply button
+	message["Reply-To"] = "User Alerter <testinguseralerterapp@gmail.com>"
+	message.preamble = "Hi"
 
+	textual_message = MIMEMultipart('alternative')
+
+	# plaintext part of the message
+	plaintext_part = MIMEText("Hello this is a test message\n testing the app", "plain")
+	textual_message.attach(plaintext_part)
+
+	# HTML part of the message
+	html_part = MIMEText("<h1>Hello this is a test message</h1><br><p style='color:red'>This is the first test message for this app! Congrats for seeing it\
+	!</p>", "html")
+	textual_message.attach(html_part)
+
+	message.attach(textual_message)
 	return message
 
 def sendEmail(message, server):
@@ -44,13 +65,19 @@ def createSSLContext(port, password):
 	return server
 
 def main():
-	port = 465 # SSL port
-	password = input("Type your password and press enter: ") # change after to using getenv variable, input, a .txt file, database or a .config file
+	# load .env variables from local .env file (using .env and dotenv to not expose credentials and other key information)
+	load_dotenv()
+
+	# get .env variables from local .env file
+	port = os.getenv("PORT") # SSL port
+	emailAddress = str(os.getenv("EMAIL")) # str may not be needed
+	password = str(os.getenv("PASSWORD")) # str may not be needed
+
 	server = createSSLContext(port, password)
-	server.login("testinguseralerterapp@gmail.com", password)
+	server.login(emailAddress, password) # FIXME use emailAddress in other functions, modularize emailWorker with ease
 	message = createEmail()
-	print(message)
-	sendEmail(message, server)
+	#print(message)
+	sendEmail(emailAddress, message, server)
 	
 	#useralerterapp1!
 	print("test")
