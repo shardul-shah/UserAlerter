ec16544d1cf31064f3ce47eb1265df4411eab40e
Shardul Shah <shardul@ualberta.ca>
2021-09-04 23:55:35 -0600

    Removing unneeded code

--- formEmailHTML.py
+++ formEmailHTML.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 
-def parseKeyInformationFromXML(webhookAlert):
+def parseXML(webhookAlert):
     keyInformation = {}
     tree = ET.parse('results.xml')
     root = tree.getroot()
@@ -25,35 +25,8 @@ def parseKeyInformationFromXML(webhookAlert):
     return
 
 def main():
-    webhookAlert = "<?xml version='1.0' encoding='UTF-8'?> \
-<feed \
-    xmlns:yt='http://www.youtube.com/xml/schemas/2015' \
-    xmlns='http://www.w3.org/2005/Atom'> \
-    <link rel='hub' href='https://pubsubhubbub.appspot.com'/> \
-    <link rel='self' href='https://www.youtube.com/xml/feeds/videos.xml?channel_id=UCSN8HjojRhNwXkpT9dK5ZZQ'/> \
-    <title>YouTube video feed</title> \
-    <updated>2021-08-31T04:21:06.194582719+00:00</updated> \
-    <entry> \
-        <id>yt:video:quCZe_N6tyQ</id>\
-        <yt:videoId>quCZe_N6tyQ</yt:videoId> \
-        <yt:channelId>UCSN8HjojRhNwXkpT9dK5ZZQ</yt:channelId> \
-        <title>test 133</title> \
-        <link rel='alternate' href='https://www.youtube.com/watch?v=quCZe_N6tyQ'/> \
-        <author> \
-            <name>5hardul League</name> \
-            <uri>https://www.youtube.com/channel/UCSN8HjojRhNwXkpT9dK5ZZQ</uri> \
-        </author> \
-        <published>2021-08-31T04:20:20+00:00</published> \
-        <updated>2021-08-31T04:21:06.194582719+00:00</updated> \
-    </entry> \
-</feed> \
-            \
-<!-- from yt video id to link: --> \
-<!-- https://www.youtube.com/watch?v=quCZe_N6tyQ --> \
-<!-- you can make further requests to Youtube API to get more information -->   "
-
     #print(webhookAlert)
-    parseKeyInformationFromXML(webhookAlert)
+    parseXML(webhookAlert)
 
 if __name__ == '__main__':
     main()
